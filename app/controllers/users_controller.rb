20053648054ecacd573f73fdee53ea24288915d9
Haixiang <haixiang6123@gmail.com>
2020-08-01 23:15:10 +0800

    feat: 添加 users Controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  def create
+    user = User.new
+
+    user.email = params[:email]
+    user.password = params[:password]
+    user.password_confirmation = params[:password_confirmation]
+
+    user.save
+
+    p user.errors
+  end
+end
