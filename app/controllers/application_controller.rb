54dd0bf09a63819d65397c13384d6386c160585c
Haixiang <haixiang6123@gmail.com>
2020-08-02 00:11:04 +0800

    refactor: 优化代码

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  def render_resources(resources)
+    if resources.errors.empty?
+      render json: {resources: resources}, status: 200
+    else
+      render json: {errors: resources.errors}, status: 400
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,9 @@
 class UsersController < ApplicationController
   def create
-    user = User.new
+    render_resources User.create create_params
+  end
 
-    user.email = params[:email]
-    user.password = params[:password]
-    user.password_confirmation = params[:password_confirmation]
-
-    if user.save
-      render json: {resouces: user}, status: 200
-    else
-      render json: {errors: user.errors}, status: 400
-    end
+  def create_params
+    params.permit(:email, :password, :password_confirmation)
   end
 end
