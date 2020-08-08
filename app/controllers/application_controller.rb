3236e1e732088bf2ea6899db45297cc649c10ac7
Haixiang <haixiang6123@gmail.com>
2020-08-08 10:19:39 +0800

    feat: 获取当前用户信息

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::API
+  def current_user
+    @current_user ||= User.find_by_id session[:current_user_id]
+  end
+
   def render_resources(resources)
+    return head 404 if resources.nil?
+
     if resources.errors.empty?
       render json: {resources: resources}, status: 200
     else
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,8 @@ class UsersController < ApplicationController
   def create_params
     params.permit(:email, :password, :password_confirmation)
   end
+
+  def me
+    render_resources current_user
+  end
 end
