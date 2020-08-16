b3fd88740bda455b37e0ccea89f7903ae14c426a
Haixiang <haixiang6123@gmail.com>
2020-08-16 22:24:44 +0800

    feat: 测试 User Model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it '可以带密码创建' do
+    user = User.create email: '1@qq.com', password: '123456', password_confirmation: '123456'
+    expect(user.password_digest).to_not eq '123456'
+    expect(user.id).to be_a Numeric
+  end
+  it '创建的时候必须有 email' do
+    user = User.create password: '123456', password_confirmation: '123456'
+    expect(user.errors.details[:email][0][:error]).to eq(:blank)
+  end
+  it '创建时 email 不能被占用' do
+    User.create! email: '1@qq.com', password: '123456', password_confirmation: '123456'
+    user = User.create email: '1@qq.com', password: '123456', password_confirmation: '123456'
+    expect(user.errors.details[:email][0][:error]).to eq(:taken)
+  end
+  it '创建之后发欢迎邮件给用户' do
+    mailer = spy('mailer')
+    allow(UserMailer).to receive(:welcome_email).and_return(mailer)
+    User.create email: '1@qq.com', password: '123456', password_confirmation: '123456'
+    expect(UserMailer).to have_received(:welcome_email)
+    expect(mailer).to have_received(:deliver_later)
+  end
 end
