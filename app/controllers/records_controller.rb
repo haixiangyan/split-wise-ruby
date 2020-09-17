79aa6ae782dd8057eb184fb135296b8efef8bf3a
Haixiang <haixiang6123@gmail.com>
2020-09-17 22:45:05 +0800

    feat: 添加删除 record

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -4,6 +4,12 @@ class RecordsController < ApplicationController
     render_resources Record.create create_params
   end
 
+  def destroy
+    must_sign_in
+    record = Record.find params[:id]
+    head record.destroy ? :ok : :bad_request
+  end
+
   def create_params
     p params
     params.permit(:amount, :category, :note)
--- spec/requests/records_request_spec.rb
+++ spec/requests/records_request_spec.rb
@@ -1,26 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe "Records", type: :request do
-  it 'should not create a record before login' do
-    post '/records', params: {amount: 10000, category: 'outgoings', note: '请客'}
-    expect(response.status).to eq 401
+  context 'create' do
+    it 'should not create a record before login' do
+      post '/records', params: {amount: 10000, category: 'outgoings', note: '请客'}
+      expect(response.status).to eq 401
+    end
+    it 'should create a record' do
+      sign_in
+      post '/records', params: {amount: 10000, category: 'outgoings', note: '请客'}
+      expect(response.status).to eq 200
+
+      body = JSON.parse(response.body)
+
+      expect(body["resources"]["id"]).to be
+    end
+    it 'should not create a record' do
+      sign_in
+      post '/records', params: {category: 'outgoings', note: '请客'}
+      expect(response.status).to eq 422
+
+      body = JSON.parse(response.body)
+
+      expect(body["errors"]["amount"][0]).to eq '金额不能为空'
+    end
   end
-  it 'should create a record' do
-    sign_in
-    post '/records', params: {amount: 10000, category: 'outgoings', note: '请客'}
-    expect(response.status).to eq 200
 
-    body = JSON.parse(response.body)
+  context 'destroy' do
+    it 'should not destroy a record before sign in' do
+      record = Record.create! amount: 10000, category: 'income'
 
-    expect(body["resources"]["id"]).to be
-  end
-  it 'should not create a record' do
-    sign_in
-    post '/records', params: {category: 'outgoings', note: '请客'}
-    expect(response.status).to eq 422
+      delete "/records/#{record.id}"
 
-    body = JSON.parse(response.body)
+      expect(response.status).to eq 401
+    end
+    it 'should destroy a record' do
+      sign_in
+      record = Record.create! amount: 10000, category: 'income'
 
-    expect(body["errors"]["amount"][0]).to eq '金额不能为空'
+      delete "/records/#{record.id}"
+
+      expect(response.status).to eq 200
+    end
   end
 end
