6fdfb99785364f0543b84f2cb70603483b088925
Haixiang <haixiang6123@gmail.com>
2020-09-18 23:48:08 +0800

    feat: 创建 taggings model

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,4 +1,6 @@
 class Record < ApplicationRecord
+  has_many :taggings # 拥有很多从属
+  has_many :tags, through: :taggings # 间接关系也有很多 tags
   enum category: { outgoings: 1, income: 2 }
   validates_presence_of :amount, :category
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_18_144100) do
+ActiveRecord::Schema.define(version: 2020_09_18_153819) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 2020_09_18_144100) do
     t.string "note"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "taggings", force: :cascade do |t|
+    t.bigint "tag_id", null: false
+    t.bigint "record_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["record_id"], name: "index_taggings_on_record_id"
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
   end
 
   create_table "tags", force: :cascade do |t|
