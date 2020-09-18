4f34b9deb04460137cec98ecab35ee59a7d1e45e
Haixiang <haixiang6123@gmail.com>
2020-09-18 22:43:11 +0800

    feat: 创建 tag 的 model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_16_154242) do
+ActiveRecord::Schema.define(version: 2020_09_18_144100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 2020_09_16_154242) do
     t.integer "amount", null: false
     t.integer "category", limit: 2, null: false
     t.string "note"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name", limit: 20
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
