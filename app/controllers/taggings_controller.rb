f0b5543f4a073e7e11cd3e95c4deb49cd128ac5c
Haixiang <haixiang6123@gmail.com>
2020-09-19 00:29:36 +0800

    feat: 测试 tagging controller

--- app/controllers/taggings_controller.rb
+++ app/controllers/taggings_controller.rb
@@ -5,7 +5,24 @@ class TaggingsController < ApplicationController
     render_resource Tagging.create create_params
   end
 
+  def destroy
+    tagging = Tagging.find params[:id]
+    head tagging.destroy ? :ok : :bad_request
+  end
+
+  def index
+    render_resources Tagging.page(params[:page])
+  end
+
+  def show
+    render_resource Tagging.find(params[:id])
+  end
+
   private
+
+  def render_resources(resources)
+    render json: {resources: resources}
+  end
 
   def create_params
     params.permit(:record_id, :tag_id)
--- spec/requests/taggings_request_spec.rb
+++ spec/requests/taggings_request_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "Taggings", type: :request do
+  before :each do
+    @record = Record.create! amount: 10000, category: 'income'
+    @tag = Tag.create! name: 'test'
+    @tagging = Tagging.create tag: @tag, record: @record
+
+    (1..11).to_a.map do |n|
+      Tagging.create! record: @record, tag: Tag.create!(name: "test#{n}")
+    end
+  end
+
   context 'create' do
     it 'should not create tagging before sign in' do
       post '/taggings'
@@ -8,9 +18,7 @@ RSpec.describe "Taggings", type: :request do
     end
     it 'should create a tagging' do
       sign_in
-      record = Record.create! amount: 10000, category: 'income'
-      tag = Tag.create! name: 'test'
-      post '/taggings', params: {record_id: record.id, tag_id: tag.id}
+      post '/taggings', params: {record_id: @record.id, tag_id: @tag.id}
       expect(response.status).to eq 200
 
       body = JSON.parse(response.body)
@@ -18,12 +26,60 @@ RSpec.describe "Taggings", type: :request do
     end
     it 'should not create a tagging without record_id' do
       sign_in
-      tag = Tag.create! name: 'test'
-      post '/taggings', params: {tag_id: tag.id}
+      post '/taggings', params: {tag_id: @tag.id}
       expect(response.status).to eq 422
 
       body = JSON.parse(response.body)
       expect(body['errors']['record'][0]).to eq '记录不能为空'
     end
   end
+
+  context "destroy" do
+    it 'should not destroy a tagging before sign in' do
+      delete "/taggings/#{@tagging.id}"
+
+      expect(response.status).to eq 401
+    end
+    it 'should destroy a tagging' do
+      sign_in
+
+      delete "/taggings/#{@tagging.id}"
+
+      expect(response.status).to eq 200
+    end
+  end
+
+  context 'index' do
+    it 'should not get tagging list before sign in' do
+      get '/taggings'
+      expect(response.status).to eq 401
+    end
+    it 'should get tag list' do
+      sign_in
+
+      get '/taggings'
+
+      expect(response.status).to eq 200
+      body = JSON.parse(response.body)
+      expect(body['resources'].length).to eq 10
+    end
+  end
+
+  context 'show' do
+    it 'should not get a tagging before sign in' do
+      get "/taggings/#{@tagging.id}"
+      expect(response.status).to eq 401
+    end
+    it 'should get a tagging' do
+      sign_in
+      get "/taggings/#{@tagging.id}"
+      expect(response.status).to eq 200
+    end
+    it 'should not found' do
+      sign_in
+      get "/taggings/9999"
+      expect(response.status).to eq 404
+    end
+  end
+
 end
